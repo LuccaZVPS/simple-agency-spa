4ec37cea72bad003323c956a324a48332b8a8790
Lucca <luccazvps@gmail.com>
2022-12-01 17:50:58 -0300

    fix: change wrong colors

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,14 +2,18 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
  
 *{
-    --BLACK: #7b4ae2;
-     --BACKGROUND: #090e16;
+    @font-face { font-family: Graphik; src: url('GraphikRegular.otf'); } 
+     @font-face { font-family: Graphik; font-weight: bold; src: url('GraphikBold.otf');;}
+    --BLACK: #0B0720;
+     --BACKGROUND: #F9F9F9;
      --GRAY:#B0BAC7;
      --BLUE:#4353FF;
      --SECONDARY:#BFC7FA;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: Graphik;
+
 }
 
 `;
