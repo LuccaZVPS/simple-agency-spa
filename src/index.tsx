f9818d8c31598522e15138ff0acfc97702d468f2
Lucca <luccazvps@gmail.com>
2022-12-01 17:11:44 -0300

    feat: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+ 
+*{
+    --PURPLE: #7b4ae2;
+     --DARK: #090e16;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+`;
