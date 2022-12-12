9ceeb3a4336430e9f7c399dbd98481ecd8236efc
Lucca <luccazvps@gmail.com>
2022-12-12 09:43:17 -0300

    feat: add content component to service section

---
+++ src/components/Service/components/Content/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Content() {
+  return (
+    <Container>
+      <h2>Our service is to make customer dream come true</h2>
+      <p>We engage in digital creativity to solve business problems</p>
+    </Container>
+  );
+}
---
+++ src/components/Service/components/Content/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 520px;
+  h2 {
+    font-size: 3.5rem;
+  }
+  p {
+    font-size: 1.1rem;
+  }
+`;
