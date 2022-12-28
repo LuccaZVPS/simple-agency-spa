3e3485c65db284429465b95c7576a083f3a16f93
Lucca Zavarize <111321889+LuccaZVPS@users.noreply.github.com>
2022-12-28 19:46:03 -0300

    Update index.tsx

--- src/components/Service/components/Grid/index.tsx
+++ src/components/Service/components/Grid/index.tsx
@@ -5,16 +5,16 @@ export function Grid() {
   return (
     <Container>
       <Item color="#DFEDFD" text="Content Creator">
-        <img src="/item2.png" alt="" />
+        <img src="/artist.png" alt="" />
       </Item>
       <Item color="#FDDFFA" text="Stragegy Planning">
-        <img src="/item2.png" alt="" />
+        <img src="/strategy.png" alt="" />
       </Item>
       <Item color="#FCFDDF" text="Product House">
-        <img src="/item2.png" alt="" />
+        <img src="/house.png" alt="" />
       </Item>
       <Item color="#DFFDF9" text="Creative Advertising">
-        <img src="/item2.png" alt="" />
+        <img src="/creative.png" alt="" />
       </Item>
     </Container>
   );
